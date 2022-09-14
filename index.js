30fcea6c72975bac9006a6d6ca913143b8a93eec
Drakhart <14996002+drakhart@users.noreply.github.com>
2022-09-14 12:51:57 +0200

    fix: read package name from package.json

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+import { readFileSync } from 'fs'
 import { DocumentStore } from 'ravendb'
 import fp from 'fastify-plugin'
 
-async function fastifyRaven(fastify, options) {
+// Workaround for ESLint not being able to parse "assert { type: 'json' }" when importing modules
+const pjsonUrl = new URL('./package.json', import.meta.url)
+const { name: packageName } = JSON.parse(readFileSync(pjsonUrl))
+
+const fastifyRaven = async (fastify, options) => {
   const {
     name,
     url,
@@ -14,12 +19,12 @@ async function fastifyRaven(fastify, options) {
 
   if (name) {
     if (documentStore[name]) {
-      throw new Error(`fastify-ravendb '${name}' is a reserved keyword`)
+      throw new Error(`${packageName} '${name}' is a reserved keyword`)
     } else if (!fastify.rvn) {
       fastify.decorate('rvn', Object.create(null))
     } else if (fastify.rvn[name]) {
       throw new Error(
-        `fastify-ravendb '${name}' instance name has already been registered`
+        `${packageName} '${name}' instance name has already been registered`
       )
     }
 
@@ -27,7 +32,7 @@ async function fastifyRaven(fastify, options) {
   } else {
     if (fastify.rvn) {
       if (fastify.rvn.initialize) {
-        throw new Error('fastify-ravendb has already been registered')
+        throw new Error(`${packageName} has already been registered`)
       }
 
       Object.assign(fastify.rvn, documentStore)
@@ -47,5 +52,5 @@ async function fastifyRaven(fastify, options) {
 
 export default fp(fastifyRaven, {
   fastify: '4.x',
-  name: 'fastify-ravendb'
+  name: packageName
 })
