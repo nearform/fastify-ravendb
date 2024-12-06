2642d31d73307c0edda835a9304feb9fee7442d3
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-12-06 10:00:31 +0100

    BREAKING CHANGE: node test runner (#360)
    * BREAKING CHANGE: test runner
    
    * chore: remote tap
    
    * chore: audit

--- test/session.test.js
+++ test/session.test.js
@@ -1,10 +1,10 @@
-import sinon from 'sinon'
-import { test } from 'tap'
-import { DocumentSession } from 'ravendb'
 import Fastify from 'fastify'
+import { test } from 'node:test'
+import { DocumentSession } from 'ravendb'
+import sinon from 'sinon'
 
 import plugin from '../index.js'
-import { url, databaseName } from './constants.js'
+import { databaseName, url } from './constants.js'
 
 class Person {
   constructor(name) {
@@ -18,7 +18,7 @@ test('Should not create any session if not enabled for route', async t => {
   await fastify.register(plugin, { url, databaseName })
 
   fastify.get('/', async req => {
-    t.notHas(req, 'rvn')
+    t.assert.ok(!req.rvn)
   })
 
   await fastify.inject({ method: 'GET', url: '/' })
@@ -32,8 +32,7 @@ test('Should create a session with no name', async t => {
   await fastify.register(plugin, { url, databaseName })
 
   fastify.get('/', { rvn: { autoSession: true } }, async req => {
-    t.hasProp(req, 'rvn')
-    t.ok(req.rvn instanceof DocumentSession)
+    t.assert.ok(req.rvn instanceof DocumentSession)
   })
 
   await fastify.inject({ method: 'GET', url: '/' })
@@ -47,9 +46,7 @@ test('Should create a session with name', async t => {
   await fastify.register(plugin, { url, databaseName, name: 'extra' })
 
   fastify.get('/', { rvn: { autoSession: 'extra' } }, async req => {
-    t.hasProp(req, 'rvn')
-    t.hasProp(req.rvn, 'extra')
-    t.ok(req.rvn.extra instanceof DocumentSession)
+    t.assert.ok(req.rvn.extra instanceof DocumentSession)
   })
 
   await fastify.inject({ method: 'GET', url: '/' })
@@ -64,11 +61,8 @@ test('Should create sessions for multiple names', async t => {
   await fastify.register(plugin, { url, databaseName, name: 'db2' })
 
   fastify.get('/', { rvn: { autoSession: ['db1', 'db2'] } }, async req => {
-    t.hasProp(req, 'rvn')
-    t.hasProp(req.rvn, 'db1')
-    t.ok(req.rvn.db1 instanceof DocumentSession)
-    t.hasProp(req.rvn, 'db2')
-    t.ok(req.rvn.db2 instanceof DocumentSession)
+    t.assert.ok(req.rvn.db1 instanceof DocumentSession)
+    t.assert.ok(req.rvn.db2 instanceof DocumentSession)
   })
 
   await fastify.inject({ method: 'GET', url: '/' })
@@ -87,8 +81,8 @@ test('Should return 500 when creating a session with a reserved name', async t =
 
   const res = await fastify.inject({ method: 'GET', url: '/' })
 
-  t.equal(res.statusCode, 500)
-  t.match(res.json().message, 'is a session reserved keyword')
+  t.assert.equal(res.statusCode, 500)
+  t.assert.match(res.json().message, /is a session reserved keyword/)
 
   await fastify.close()
 })
@@ -130,8 +124,8 @@ test('Should create a session respecting existing handlers', async t => {
     '/',
     { rvn: { autoSession: true }, preHandler: async () => {} },
     async req => {
-      t.ok(Array.isArray(req.context.preHandler))
-      t.equal(req.context.preHandler.length, 2)
+      t.assert.ok(Array.isArray(req.context.preHandler))
+      t.assert.equal(req.context.preHandler.length, 2)
     }
   )
 
